d5949ceb927f92bbfabf88a6e7e66836f700678e
Aaron Garza <robertaarongarza@gmail.com>
2017-01-11 16:58:55 -0500

    Update invalid move function.

--- towers.rb
+++ towers.rb
@@ -18,36 +18,30 @@
 
 
 def play(disc_num, columns)
-  columns[0] = (1..disc_num).to_a.reverse
+  columns[0] = (1..disc_num).to_a
   columns[1] = []
   columns[2] = []
-  render_columns(columns)
-    while(true)
-      
-      if(check_win(columns))
-        puts "You win!"
-        exit
-      end
-
+  render(columns)
+    until check_win(columns) == true
       puts "Enter Move >"
       input = gets.chomp
-
       if input == "q"
         exit
+      # elsif invalid_move == true
+      #   puts "Invalid move."
+      else
+        input = input.split(",")  
+        move_from = input[0].to_i - 1
+        move_to = input[1].to_i - 1
+        move_disc(columns,move_from,move_to)
+        render(columns)
       end
-      # Check input for legal input
-      # a,b move a to b
-      input = input.split(',')
-      
-      move_from = input[0].to_i - 1
-      move_to = input[1].to_i - 1
-      
-      move_disc(columns,move_from,move_to)
-      render_columns(columns)
     end
+    puts "You win!"
+      exit
 end
 
-def render_columns(columns)
+def render(columns)
   puts "Column 1"
   puts columns[0]
   puts "Column 2"
@@ -56,31 +50,19 @@ def render_columns(columns)
   puts columns[2]
 end 
 
-def move_disc(columns,move_from,move_to)
-  # if invalid_move is false then do move
-  # else print an error
+def move_disc(columns, move_from, move_to)
   columns[move_to] << columns[move_from].pop
 end 
 
-def invalid_move
-  # Suppose (a,b) is user input
-  # Illegal if the block at top of column b is smaller than a
-  # or top of a is smaller than top of b
-  # Strictly illegal if column a is empty.
-  # Legal if column b is empty.
-  if from > to
-    return "Invalid move."
-  end 
-  if from.empty?
-    return "Invalid move"
-  end
-end 
+# def invalid_move(columns)
+#   columns[move_from].empty?
+# end 
 
 def check_win(columns)
    return columns[0].empty? && columns[1].empty?
 end
 
-def main
+def start
   hanoi = []
   puts "Welcome to Tower of Hanoi!"
   puts "Instructions:"
@@ -88,8 +70,7 @@ def main
   puts "in the format '1,3'. Enter 'q' to quit."
   puts "How many discs to use?"
   disc_num = gets.chomp.to_i
-  # Input check? makes sure disc_num is positive integer
   play(disc_num, hanoi)
 end
 
-main()
+start()
